51e7275287663de9e370a1df7c88559ef8279308
Isabellebrian <Isabellebrian@gmail.com>
2023-09-11 20:11:36 +1000

    Initial Commit

---
+++ AssessmentTask2/AssessmentTask2App.swift
@@ -0,0 +1,17 @@
+//
+//  AssessmentTask2App.swift
+//  AssessmentTask2
+//
+//  Created by Isabelle Brian on 11/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssessmentTask2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
